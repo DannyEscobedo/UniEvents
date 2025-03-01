47657e5ea54c7097fed2e1f7570b5289f2166cb4
Daniela Escobedo <134535590+DannyEscobedo@users.noreply.github.com>
2025-02-28 18:59:12 -0600

    MenuAdministrador.php

--- MenuAdministrador.php
+++ MenuAdministrador.php
@@ -1,3 +1,16 @@
+<?php
+session_start();  // Inicia la sesión
+
+// Verifica si la sesión está activa
+if (!isset($_SESSION["usuario"])) {
+    // Si no está autenticado, redirige a la página de inicio de sesión
+    header("Location: IniciarSesion.php");
+    exit();  // Detiene la ejecución para evitar mostrar el contenido de la página
+}
+
+// Si la sesión está activa, el contenido del menú se muestra
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -6,6 +19,6 @@
 </head>
 <body>
     <h1>¡Bienvenido de nuevo, Administrador@!</h1>
-    
+    <a href="CerrarSesion.php">Cerrar sesión</a>  <!-- Enlace para cerrar sesión -->
 </body>
 </html>
