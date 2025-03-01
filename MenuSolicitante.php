b8ddaad8345cfb31368f2cc7c829db69734dc0ec
Daniela Escobedo <134535590+DannyEscobedo@users.noreply.github.com>
2025-02-28 19:00:26 -0600

    MenuSolicitante.php

--- MenuSolicitante.php
+++ MenuSolicitante.php
@@ -1,3 +1,15 @@
+<?php
+session_start();  // Inicia la sesión
+
+// Verifica si la sesión está activa
+if (!isset($_SESSION["usuario"])) {
+    // Si no está autenticado, redirige a la página de inicio de sesión
+    header("Location: IniciarSesion.php");
+    exit();  // Detiene la ejecución para evitar mostrar el contenido de la página
+}
+
+// Si la sesión está activa, el contenido del menú se muestra
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -5,7 +17,7 @@
     <title>Menú Solicitante</title>
 </head>
 <body>
-    <h1>¡Bienvenido de nuevo, alumn@!</h1>
-    
+    <h1>¡Bienvenido de nuevo, Alumn@!</h1>  
+     <a href="CerrarSesion.php">Cerrar sesión</a>  <!-- Enlace para cerrar sesión --> 
 </body>
 </html>
