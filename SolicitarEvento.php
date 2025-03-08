a7a450634d1dc2e1243157f5817726d1aa57a3bc
Daniela Escobedo <134535590+DannyEscobedo@users.noreply.github.com>
2025-03-07 19:30:41 -0600

    SolicitarEvento.php

---
+++ SolicitarEvento.php
@@ -0,0 +1,9 @@
+<?php
+session_start();  // Inicia la sesión
+
+// Verifica si la sesión está activa
+if (!isset($_SESSION["usuario"])) {
+    header("Location: IniciarSesion.php"); // Redirige al login si no hay sesión
+    exit();
+}
+?>
