dd4a31876d49b7fc505af7dc2a73bb881ba2648b
Daniela Escobedo <134535590+DannyEscobedo@users.noreply.github.com>
2025-03-30 01:35:34 -0600

    RestablecerContraseña.php

--- RestablecerContraseña.php
+++ RestablecerContraseña.php
@@ -27,6 +27,7 @@
     </div>
 
     <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap');
         p {
             color: black;
             text-align: center;
